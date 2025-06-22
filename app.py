ccd6472588eaec79e9967b6663f6ff71f9945a7c
Yash sharma <44648735+Yash9808@users.noreply.github.com>
2025-06-22 10:45:58 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+# app.py
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            # OPTIONAL: Add your Mediapipe processing here
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # HTML file that shows webcam
+
+@app.route('/video')
+def video():
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
